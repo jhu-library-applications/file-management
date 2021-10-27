4dc78d0e492e74a2da2bf9cd5562fbbf23c84dd0
Michelle Janowiecki <32551917+mjanowiecki@users.noreply.github.com>
2021-10-27 09:29:17 -0400

    changes

--- extractFileAndFolderNamesInDirectory.py
+++ extractFileAndFolderNamesInDirectory.py
@@ -1,35 +1,34 @@
 import csv
 import os
 import time
+from datetime import datetime
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-d', '--directory', help='the directory of the fileNames to be extracted. optional - if not provided, the script will ask for input')
-parser.add_argument('-f', '--fileExtension', help='the file extension of file names to be extracted. optional - if not provided, the script will ask for input')
+parser.add_argument('-p', '--filePath', help='File path (C:/Test/)')
+parser.add_argument('-f', '--fileExtension', help='File extensions')
 args = parser.parse_args()
 
-if args.directory:
-    directory = args.directory
+if args.filePath:
+    filePath = args.filePath
 else:
-    directory = input('Enter file path (C:/Test/): ')
+    filePath = input('Enter file path (C:/Test/): ')
 if args.fileExtension:
     fileExtension = args.fileExtension
 else:
-    fileExtension = input('Enter file extension like jpg or txt (no period before extension): ')
+    fileExtension = input('Enter file extension like jpg or txt (no period): ')
 
-directoryName = directory.replace('/', '')
-directoryName = directoryName.replace(':', '')
-print(directoryName)
-
+dt = datetime.now().strftime('%Y-%m-%d%H.%M.%S')
 startTime = time.time()
-f = csv.writer(open(directoryName+'Listing'+'.csv', 'w'))
-f.writerow(['currentFilePath']+['fileName']+['newFileName'])
-directories = os.walk(directory, topdown=True)
-for filePath, subFolders, fileNames in directories:
-    for fileName in fileNames:
-        if fileName[-3:] == fileExtension:
-            print(filePath, fileName)
-            f.writerow([filePath]+[fileName])
+f = csv.writer(open('filesAndFoldersListing_'+dt+'.csv', 'w'))
+f.writerow(['currentFilePath']+['fileName'])
+directories = os.walk(filePath, topdown=True)
+for root, dirs, files in directories:
+    for filename in files:
+        print(filename)
+        if filename[-3:] == fileExtension:
+            print(filePath, filename)
+            f.writerow([filePath]+[filename])
 
 elapsedTime = time.time() - startTime
 m, s = divmod(elapsedTime, 60)
--- extractFilenames.py
+++ extractFilenames.py
@@ -1,24 +1,31 @@
 import csv
 import os
 import time
+from datetime import datetime
 
-filePath = input('Enter file path (C:/Test/): ')
-fileExtension = input('Enter file extension: ')
-prefix = input('Enter prefix to be added: ')
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--filePath', help='File path (C:/Test/)')
+parser.add_argument('-f', '--fileExtension', help='File extensions')
+args = parser.parse_args()
 
+if args.filePath:
+    filePath = args.filePath
+else:
+    filePath = input('Enter file path (C:/Test/): ')
+if args.fileExtension:
+    fileExtension = args.fileExtension
+else:
+    fileExtension = input('Enter file extension like jpg or txt (no period): ')
+
+dt = datetime.now().strftime('%Y-%m-%d%H.%M.%S')
 startTime = time.time()
-f = csv.writer(open('fileListing.csv', 'w'))
-f.writerow(['file'] + ['newFile'])
-f2 = csv.writer(open('fullfileListing.csv', 'w'))
-f2.writerow(['file'] + ['newFile'])
+f = csv.writer(open('filesListing_'+dt+'.csv', 'w'))
+f.writerow(['filename'])
 directories = os.walk(filePath, topdown=True)
 for root, dirs, files in directories:
     for file in files:
-        f2.writerow([file])
         if file[-3:] == fileExtension:
-            itemID = file[0:5]
-            newFile = prefix + itemID + fileExtension
-            f.writerow([file] + [newFile])
+            f.writerow([file])
 
 elapsedTime = time.time() - startTime
 m, s = divmod(elapsedTime, 60)
